a8f5fd6b0ada637344e78eaa693385f7e539da37
Harsh Ankur <harshankur@outlook.com>
2019-04-19 22:35:50 +0900

    Support for parsing text from excel file added

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 A Node.js library to parse text out of any office file. 
 
 ~~Currently supports docx. Support for pptx and xlsx coming soon~~
-*Currently supports docx and pptx. xlsx support coming soon.*
+~~Currently supports docx and pptx. xlsx support coming soon.~~
+
+##### Update
+*Supports docx, pptx and xlsx files. Investigating possibilities of fetching text from doc, ppt, pps, xls files.*
 
 
 
@@ -28,6 +31,11 @@ officeParser.parsePowerPoint("/path/to/powerpoint.pptx", function(data){
         // "data" string in the callback here is the text parsed from the powerpoint file passed in the first argument above
         console.log(data)
 })
+
+officeParser.parseExcel("/path/to/excel.xlsx", function(data){
+        // "data" string in the callback here is the text parsed from the excel file passed in the first argument above
+        console.log(data)
+})
 ```
 
 **Example**
@@ -39,8 +47,14 @@ officeParser.parseWord("C:\\files\\myText.docx", function(data){
         callSomeOtherFunction(newText);
 })
 
-officeParser.parsePowerPoint("/Users/harsh/Desktop/files/powerpoint.pptx", function(data){
+officeParser.parsePowerPoint("/Users/harsh/Desktop/files/mySlides.pptx", function(data){
         var newText = data + "look, I can parse a powerpoint file"
+        callSomeOtherFunction(newText);
+})
+
+// Using relative path for file is also fine
+officeParser.parseExcel("files/myWorkSheet.xlsx", function(data){
+        var newText = data + "look, I can parse an excel file"
         callSomeOtherFunction(newText);
 })
 ```
--- officeParser.js
+++ officeParser.js
@@ -128,5 +128,150 @@ var parsePowerPoint = function(filename, callback) {
 // #endregion textFetchFromPowerPoint
 
 
+// #region textFetchFromExcel
+
+var myTextExcel = [];
+
+async function scanForTextExcelDrawing(result) {
+    if (Array.isArray(result)) {
+        for (var i = 0; i < result.length; i++) {
+            if (typeof(result[i]) == "string" && result[i] != "") {
+                await myTextExcel.push(result[i]);
+            }
+            else {
+                await scanForTextExcelDrawing(result[i]);
+            }
+        }
+        return;
+    }
+    else if(typeof(result) == "object") {
+        for (var property in result) {
+            if (result.hasOwnProperty(property)) {
+                if (typeof(result[property]) == "string"){
+                    if ((property == "a:t" || property == "_") && result[property] != "") {
+                        await myTextExcel.push(result[property]);
+                    }
+                }
+                else if (typeof(result[property][0]) == "string"){
+                    if ((property == "a:t" || property == "_") && result[property] != "") {
+                        await myTextExcel.push(result[property]);
+                    }
+                }
+                else {
+                    await scanForTextExcelDrawing(result[property]);
+                }
+            }
+        }
+        return;
+    }
+}
+
+
+async function scanForTextExcelSharedStrings(result) {
+    if (Array.isArray(result)) {
+        for (var i = 0; i < result.length; i++) {
+            if (typeof(result[i]) == "string" && result[i] != "") {
+                await myTextExcel.push(result[i]);
+            }
+            else {
+                await scanForTextExcelSharedStrings(result[i]);
+            }
+        }
+        return;
+    }
+    else if(typeof(result) == "object") {
+        for (var property in result) {
+            if (result.hasOwnProperty(property)) {
+                if (typeof(result[property]) == "string"){
+                    if ((property == "t" || property == "_") && result[property] != "") {
+                        await myTextExcel.push(result[property]);
+                    }
+                }
+                else if (typeof(result[property][0]) == "string"){
+                    if ((property == "t" || property == "_") && result[property] != "") {
+                        await myTextExcel.push(result[property]);
+                    }
+                }
+                else {
+                    await scanForTextExcelSharedStrings(result[property]);
+                }
+            }
+        }
+        return;
+    }
+}
+
+async function scanForTextExcelWorkSheet(result) {
+    if (Array.isArray(result)) {
+        for (var i = 0; i < result.length; i++) {
+            if (result[i]["v"]) {
+                if ((result[i]["$"]["t"] != "s")) {
+                    await myTextExcel.push(result[i]["v"][0]);
+                }
+            }
+            else {
+                await scanForTextExcelWorkSheet(result[i]);
+            }
+        }
+        return;
+    }
+    else if(typeof(result) == "object") {
+        for (var property in result) {
+            if (result.hasOwnProperty(property)) {
+                if (result[property]["v"]) {
+                    if ((result[property]["$"]["t"] == "s")) {
+                        await myTextExcel.push(result[property]["v"][0]);
+                    }
+                }
+                else {
+                    await scanForTextExcelWorkSheet(result[property]);
+                }
+            }
+        }
+        return;
+    }
+}
+
+var parseExcel = function(filename, callback) {
+    decompress(filename, 'dist').then(async files => {
+        myTextExcel = [];
+
+        var workSheetsNum = await fs.readdirSync('dist/xl/worksheets').length;
+        var drawingsNum = await fs.readdirSync('dist/xl/drawings').length;
+        console.log(workSheetsNum);
+        console.log(drawingsNum);
+
+        for (var i=0; i<workSheetsNum-1; i++) {
+            var parser = new xml2js.Parser();
+
+            var myData = await util.promisify(fs.readFile)(`dist/xl/worksheets/sheet${i+1}.xml`, 'utf8');
+            var result = await util.promisify(parser.parseString.bind(parser))(myData);
+            await scanForTextExcelWorkSheet(result);
+        }
+        console.log(result["worksheet"]["sheetData"][0]["row"][4]["c"][0]["v"][0]);
+
+
+        var parser = new xml2js.Parser();
+
+        var myData = await util.promisify(fs.readFile)(`dist/xl/sharedStrings.xml`, 'utf8');
+        var result = await util.promisify(parser.parseString.bind(parser))(myData);
+        await scanForTextExcelSharedStrings(result);
+
+
+        for (var i=0; i<drawingsNum; i++) {
+            var parser = new xml2js.Parser();
+
+            var myData = await util.promisify(fs.readFile)(`dist/xl/drawings/drawing${i+1}.xml`, 'utf8');
+            var result = await util.promisify(parser.parseString.bind(parser))(myData);
+            await scanForTextExcelDrawing(result);
+        }
+
+        callback(myTextExcel.join(" "));
+    });
+} 
+
+// #endregion textFetchFromExcel
+
 module.exports.parseWord = parseWord;
 module.exports.parsePowerPoint = parsePowerPoint;
+module.exports.parseExcel = parseExcel;
